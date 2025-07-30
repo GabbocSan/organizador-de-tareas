6b1a8ab13b532ce6d433bea175279c5b2636ab65
GabbocSan <gabocruzsanabria@gmail.com>
2025-07-30 17:28:13 -0400

    Se organizo mejor el codigo

--- src/App.jsx
+++ src/App.jsx
@@ -1,49 +1,15 @@
-import { useState } from "react";
-import React from "react";
+import { AddTask } from "./AddTask";
 
 function App() {
 
-  const [tasks, setTasks] = useState([]);
-  const [newTaskText, setNewTaskText] = useState('');
-  const [editingId, setEditingId] = useState(null);
-  const [editText, setEditText] = useState('');
-
-  const addTask = () => {
-  if (newTaskText.trim()) {
-    const newTask = {
-      id: Date.now(), // ID único basado en timestamp
-      text: newTaskText.trim(),
-      completed: false,
-      createdAt: new Date().toLocaleDateString()
-    };
-    setTasks([...tasks, newTask]);
-    setNewTaskText('');
-  }
-};
+  //const [editingId, setEditingId] = useState(null);
+  //const [editText, setEditText] = useState('');
 
   return (
     <>
       <div>
         <h1>Organizador de tareas</h1>
-        <div>
-          <input
-            type="text"
-            value={newTaskText}
-            onChange={(e) => setNewTaskText(e.target.value)}
-            placeholder="Escribe una nueva tarea..."
-          />
-          <button onClick={addTask}>
-            Agregar
-          </button>
-        </div>
-        <div>
-          {tasks.map((task) => (
-            <div key={task.id}>
-              <p>{task.text}</p>
-              <p>Creada: {task.createdAt}</p>
-            </div>
-          ))}
-        </div>
+        <AddTask/>
       </div>
       
     </>
